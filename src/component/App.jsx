e19ace5af3f641b0f2163864dcfd5ecbfdc80f32
zakir1999 <m.zakirhossen99@gmail.com>
2025-03-18 10:28:59 +0600

    solve this problem

--- src/component/App.jsx
+++ src/component/App.jsx
@@ -1,10 +1,12 @@
 import "../Styles/App.css";
 import Layout from "./Layout.jsx";
+import Home from "./Pages/Home.jsx";
 import Signup from "./Pages/Signup.jsx";
 function App() {
   return (
     <Layout>
-      <Signup></Signup>
+      <Home />
+      <Signup />
     </Layout>
   );
 }
--- src/component/TextInput.jsx
+++ src/component/TextInput.jsx
@@ -1,6 +1,6 @@
 import classes from "../Styles/TextInput.module.css";
 
-export default function TextInput(icon, ...rest) {
+export default function TextInput({ icon, ...rest }) {
   return (
     <div className={classes.textInput}>
       <input {...rest} />
