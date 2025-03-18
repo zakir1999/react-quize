e4167a9e3163c6162a2c9eaa122a53de04f2b53b
zakir1999 <m.zakirhossen99@gmail.com>
2025-03-18 13:03:37 +0600

    Lession-7 Completed

--- src/component/App.jsx
+++ src/component/App.jsx
@@ -1,13 +1,15 @@
 import "../Styles/App.css";
 import Layout from "./Layout.jsx";
-import Quize from "./Pages/Quize";
+// import Quize from "./Pages/Quize";
 // import Home from "./Pages/Home";
 // import SignIn from "./Pages/SignIn.jsx";
+import Result from "./Pages/Result";
 function App() {
   return (
     <Layout>
+      <Result />
       {/* <Home></Home> */}
-      <Quize />
+      {/* <Quize /> */}
       {/* <Signup /> */}
 
       {/* <SignIn></SignIn> */}
